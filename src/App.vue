607ffcd57794e1508360778ed2f67950b2490a58
王呵呵 <1045735119@qq.com>
2020-09-08 20:44:11 +0800

    更新后的文件

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <p>我是小哥哥  小姐姐网恋吗？</p>
+    <p>
+      少年 你渴望力量吗？
+    </p>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,12 +1,24 @@
 <template>
   <div>
     小哥哥  网恋吗？
+    <p>
+      我还是 个孩子啊
+    </p>
+    <ul>
+      <li v-for="(item,index) in list" :key="index">
+          {{item}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return{
+      list:[1,2,3,5,6,8,9,10],
+    }
+  }
 }
 </script>
 
